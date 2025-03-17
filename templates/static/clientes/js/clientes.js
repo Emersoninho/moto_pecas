f7e3a01b8c27c20e03e8c3aa3b5c773b7e21306e
emerson <emersoneletrotecnico2013@hotmail.com>
2025-03-17 17:00:08 -0300

    html de clientes e o js
    html de clientes e java scripts

---
+++ templates/static/clientes/js/clientes.js
@@ -0,0 +1,20 @@
+function add_carro() {
+    var container = document.getElementById("form-carro");
+
+    var html = `
+        <br>
+        <div class="row">
+            <div class="col-md">
+                <input type="text" placeholder="Moto" class="form-control" name="carro">
+            </div>
+            <div class="col-md">
+                <input type="text" placeholder="Placa" class="form-control" name="placa">
+            </div>
+            <div class="col-md">
+                <input type="number" placeholder="Ano" class="form-control" name="ano">
+            </div>
+        </div>
+    `;
+
+    container.innerHTML += html;
+}
