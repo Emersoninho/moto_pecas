f57dc004c0f057c5c28920a611f3331b348f6cc2
emerson <emersoneletrotecnico2013@hotmail.com>
2025-03-17 22:04:57 -0300

    views
    atualização das views

--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def clientes(request):
-    return render(request, 'clientes/clientes.html')
+    if request.method == 'GET':
+        return render(request, 'clientes/clientes.html')
+    elif request.method == 'POST':
+        name = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        carros = request.POST.getlist('carro')
+        placas = request.POST.getlist('placa')
+        anos = request.POST.getlist('ano')
