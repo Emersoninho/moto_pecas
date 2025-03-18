b6e85a24ff78789f534542608a6602a9bb6278e9
emerson <emersoneletrotecnico2013@hotmail.com>
2025-03-17 22:25:32 -0300

    models
    criando a models dos clientes

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    cpf = models.IntegerField(max_length=12)
+
+    def __str__(self):
+        return self.first_name
+
+class Carro(models.Model):
+    carro = models.CharField(max_length=50)
+    placa = models.CharField(max_length=8)
+    ano = models.IntegerField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    lavagens = models.IntegerField(default=0)
+    consertos = models.IntegerField(default=0)        
